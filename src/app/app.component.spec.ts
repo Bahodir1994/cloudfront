beb17afb1af26df9e7d93d16ffa256fc1afb7f31
bahodir <imomuddinov@inbox.ru>
2024-01-26 18:24:46 +0500

    add v2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'cloudfront' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('cloudfront');
-  });
+  // it(`should have the 'cloudfront' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('cloudfront');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/components/custom-sidenav/custom-sidenav.component.ts
+++ src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -3,6 +3,14 @@ import {CommonModule} from "@angular/common";
 import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {MatExpansionModule, MatExpansionPanel} from "@angular/material/expansion";
+
+export type BlobalMenu = {
+  icon: string;
+  label: string;
+  route: string;
+  items: [];
+}
 
 export type MenuItem = {
   icon: string;
@@ -18,13 +26,15 @@ export type MenuItem = {
     MatListModule,
     MatIconModule,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatExpansionModule
   ],
   templateUrl: './custom-sidenav.component.html',
   styleUrl: './custom-sidenav.component.css'
 })
 export class CustomSidenavComponent {
   sideNavCollapsed = signal(false)
+
   @Input() set collapsed(val: boolean){
     this.sideNavCollapsed.set(val)
   }
@@ -32,25 +42,25 @@ export class CustomSidenavComponent {
   profilePicSize = computed(() => this.sideNavCollapsed() ? '32':'100')
 
   menuItems = signal<MenuItem[]>([
-    {
-      icon: 'dashboard',
-      label: 'БКО',
-      route: 'tp-order',
-    },
-    {
-      icon: 'attach_file',
-      label: 'Мурожаатлар',
-      route: 'tp-appeals',
-    },
-    {
-      icon: 'pie_chart',
-      label: 'Хисоботлар',
-      route: 'tp-report',
-    },
-    {
-      icon: 'local_offer',
-      label: 'Маълумотнома',
-      route: 'tp-catalog',
-    },
+        {
+          icon: 'dashboard',
+          label: 'БКО',
+          route: 'tp-order',
+        },
+        {
+          icon: 'attach_file',
+          label: 'Мурожаатлар',
+          route: 'tp-appeals',
+        },
+        {
+          icon: 'pie_chart',
+          label: 'Хисоботлар',
+          route: 'tp-report',
+        },
+        {
+          icon: 'local_offer',
+          label: 'Маълумотнома',
+          route: 'tp-catalog',
+        },
   ])
 }
